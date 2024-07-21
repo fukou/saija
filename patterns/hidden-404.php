b61fc9a73a49537c94f09009ea6239be2799b19b
Faiz <faiz.j@softwareseni.com>
2024-07-21 13:00:40 +0700

    feat(patterns): Adjust the 404 not found pattern

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -8,12 +8,38 @@
  * @since 1.0.0
  */
 ?>
-<!-- wp:heading {"textAlign":"center", "level": 1} -->
-<h1 class="wp-block-heading has-text-align-center">
-	<?php esc_html_e( 'Page not found', 'saija' ); ?>
-</h1>
-<!-- /wp:heading -->
-<!-- wp:paragraph -->
-<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'saija' ); ?></p>
-<!-- /wp:paragraph -->
-<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'Search form label', 'saija' ); ?>","showLabel":false,"buttonText":"<?php echo esc_html_x( 'Search', 'Search form submit button text', 'saija' ); ?>"} /-->
+
+<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"0","bottom":"0"}},"dimensions":{"minHeight":"100%"}},"layout":{"type":"default"}} -->
+<main class="wp-block-group" style="min-height:100%;padding-top:0;padding-bottom:0">
+  <!-- wp:group {"align":"full","className":"is-style-saija-marquee-right is-style-not-found","layout":{"type":"flex","flexWrap":"wrap"}} -->
+  <div class="wp-block-group alignfull is-style-saija-marquee-right is-style-not-found">
+    <!-- wp:heading {"textAlign":"left","level":1,"align":"full","style":{"typography":{"fontSize":"8rem"}}} -->
+    <h1 class="wp-block-heading alignfull has-text-align-left" style="font-size:8rem">Error 404</h1>
+    <!-- /wp:heading -->
+
+    <!-- wp:heading {"textAlign":"left","level":1,"align":"full","style":{"typography":{"fontSize":"8rem"}}} -->
+    <h1 class="wp-block-heading alignfull has-text-align-left" style="font-size:8rem">Page not found</h1>
+    <!-- /wp:heading -->
+
+    <!-- wp:heading {"textAlign":"left","level":1,"align":"full","style":{"typography":{"fontSize":"8rem"}}} -->
+    <h1 class="wp-block-heading alignfull has-text-align-left" style="font-size:8rem">Try again</h1>
+    <!-- /wp:heading -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+  <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60)"><!-- wp:paragraph -->
+    <p>It looks like nothing was found at this location. </p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:buttons -->
+    <div class="wp-block-buttons"><!-- wp:button -->
+      <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="/">Back to home</a></div>
+      <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+  </div>
+  <!-- /wp:group -->
+</main>
+<!-- /wp:group -->
+ 
