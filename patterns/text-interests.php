2e3c98bd8431c841e76b5b24faf0c96e73a525f4
Faiz <faiz.j@softwareseni.com>
2024-07-21 12:05:26 +0700

    feat(patterns): add lists of interest block pattern

---
+++ patterns/text-interests.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Title: Lists of interests
+ * Slug: saija/text-interests
+ * Categories: text, about, featured
+ * Viewport width: 1400
+ */
+?>
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|40"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0;padding-bottom:var(--wp--preset--spacing--40)">
+  <!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"x-large","fontFamily":"body"} -->
+  <h3 class="wp-block-heading has-body-font-family has-x-large-font-size" style="font-style:normal;font-weight:400">My focus is on creating a balanced 🤹🏻‍♂️ combination of good design and user experiences. I am passionate about accessibility ♿ design and writing good 👨🏻‍💻 code.</h3>
+  <!-- /wp:heading -->
+
+  <!-- wp:group {"metadata":{"name":"Lists of interests"},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+  <div class="wp-block-group">
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"large","fontFamily":"heading","layout":{"type":"default"}} -->
+    <div class="wp-block-group has-heading-font-family has-large-font-size"
+      style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
+      <!-- wp:paragraph -->
+      <p>User interface design and experience</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"color":"#a9a9a9","width":"1px"},"right":{},"bottom":{},"left":{}}},"fontSize":"large","fontFamily":"heading","layout":{"type":"default"}} -->
+    <div class="wp-block-group has-heading-font-family has-large-font-size"
+      style="border-top-color:#a9a9a9;border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
+      <!-- wp:paragraph -->
+      <p>Project management</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"color":"#a9a9a9","width":"1px"},"right":{},"bottom":{},"left":{}}},"fontSize":"large","fontFamily":"heading","layout":{"type":"default"}} -->
+    <div class="wp-block-group has-heading-font-family has-large-font-size"
+      style="border-top-color:#a9a9a9;border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
+      <!-- wp:paragraph -->
+      <p>Visual design</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"color":"#a9a9a9","width":"1px"},"right":{},"bottom":{},"left":{}}},"fontSize":"large","fontFamily":"heading","layout":{"type":"default"}} -->
+    <div class="wp-block-group has-heading-font-family has-large-font-size"
+      style="border-top-color:#a9a9a9;border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
+      <!-- wp:paragraph -->
+      <p>Experimentation with technology</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
