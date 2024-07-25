26ec6a65b39a7e0315d2b93ad1421d068b9ea59b
Faiz <faiz.j@softwareseni.com>
2024-07-25 21:21:35 +0700

    feat(patterns): Adjust the text interests block patterns

--- patterns/text-interests.php
+++ patterns/text-interests.php
@@ -9,45 +9,47 @@
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|40"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group alignwide" style="padding-top:0;padding-bottom:var(--wp--preset--spacing--40)">
-  <!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"x-large","fontFamily":"body"} -->
-  <h3 class="wp-block-heading has-body-font-family has-x-large-font-size" style="font-style:normal;font-weight:400">My focus is on creating a balanced 🤹🏻‍♂️ combination of good design and user experiences. I am passionate about accessibility ♿ design and writing good 👨🏻‍💻 code.</h3>
+  <!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"x-large","fontFamily":"favorit-tumblr"} -->
+  <h3 class="wp-block-heading has-favorit-tumblr-font-family has-x-large-font-size"
+    style="font-style:normal;font-weight:300">My focus is on creating a balanced 🤹🏻‍♂️ combination of good design
+    and user experiences. I am passionate about accessibility ♿ design and writing good 👨🏻‍💻 code.</h3>
   <!-- /wp:heading -->
 
   <!-- wp:group {"metadata":{"name":"Lists of interests"},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
   <div class="wp-block-group">
-    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"large","fontFamily":"heading","layout":{"type":"default"}} -->
-    <div class="wp-block-group has-heading-font-family has-large-font-size"
-      style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
-      <!-- wp:paragraph -->
-      <p>User interface design and experience</p>
-      <!-- /wp:paragraph -->
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"width":"1px"}}},"fontSize":"large","layout":{"type":"default"}} -->
+    <div class="wp-block-group has-large-font-size"
+      style="border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
+      <!-- wp:heading -->
+      <h2 class="wp-block-heading">User interface design and experience</h2>
+      <!-- /wp:heading -->
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"color":"#a9a9a9","width":"1px"},"right":{},"bottom":{},"left":{}}},"fontSize":"large","fontFamily":"heading","layout":{"type":"default"}} -->
-    <div class="wp-block-group has-heading-font-family has-large-font-size"
-      style="border-top-color:#a9a9a9;border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
-      <!-- wp:paragraph -->
-      <p>Project management</p>
-      <!-- /wp:paragraph -->
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"width":"1px"}}},"fontSize":"large","layout":{"type":"default"}} -->
+    <div class="wp-block-group has-large-font-size"
+      style="border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
+      <!-- wp:heading -->
+      <h2 class="wp-block-heading">Project management</h2>
+      <!-- /wp:heading -->
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"color":"#a9a9a9","width":"1px"},"right":{},"bottom":{},"left":{}}},"fontSize":"large","fontFamily":"heading","layout":{"type":"default"}} -->
-    <div class="wp-block-group has-heading-font-family has-large-font-size"
-      style="border-top-color:#a9a9a9;border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
-      <!-- wp:paragraph -->
-      <p>Visual design</p>
-      <!-- /wp:paragraph -->
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"width":"1px"}}},"fontSize":"large","layout":{"type":"default"}} -->
+    <div class="wp-block-group has-large-font-size"
+      style="border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
+      <!-- wp:heading -->
+      <h2 class="wp-block-heading">Visual design</h2>
+      <!-- /wp:heading -->
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"color":"#a9a9a9","width":"1px"},"right":{},"bottom":{},"left":{}}},"fontSize":"large","fontFamily":"heading","layout":{"type":"default"}} -->
-    <div class="wp-block-group has-heading-font-family has-large-font-size"
-      style="border-top-color:#a9a9a9;border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
-      <!-- wp:paragraph -->
-      <p>Experimentation with technology</p>
-      <!-- /wp:paragraph -->
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"border":{"top":{"width":"1px"}}},"fontSize":"large","layout":{"type":"default"}} -->
+    <div class="wp-block-group has-large-font-size"
+      style="border-top-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);font-style:normal;font-weight:600">
+      <!-- wp:heading -->
+      <h2 class="wp-block-heading">Experimentation with technology</h2>
+      <!-- /wp:heading -->
     </div>
     <!-- /wp:group -->
   </div>
