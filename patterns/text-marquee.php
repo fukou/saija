3fa0da943edc7e208bac55f28ede9381322fb214
Faiz <faiz.j@softwareseni.com>
2024-07-25 21:21:52 +0700

    feat(patterns): Add the text marquee block patterns

---
+++ patterns/text-marquee.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Marquee text
+ * Slug: saija/text-marquee
+ *
+ * @package saija
+ * @since 1.0.0
+ */
+?>
+
+<!-- wp:group {"align":"full","className":"is-style-saija-marquee-right","style":{"spacing":{"padding":{"top":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull is-style-saija-marquee-right has-contrast-color has-text-color has-link-color"
+  style="padding-top:var(--wp--preset--spacing--20)">
+  <!-- wp:heading {"align":"full","style":{"typography":{"fontSize":"9rem"}},"textColor":"white"} -->
+  <h2 class="wp-block-heading alignfull has-white-color has-text-color" style="font-size:9rem"><a
+      href="https://www.linkedin.com/in/faizichsanjaya/">LinkedIn</a></h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:heading {"align":"full","style":{"typography":{"fontSize":"9rem"}},"textColor":"white"} -->
+  <h2 class="wp-block-heading alignfull has-white-color has-text-color" style="font-size:9rem"><a
+      href="https://github.com/fukou">Github</a></h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:heading {"align":"full","style":{"typography":{"fontSize":"9rem"}},"textColor":"white"} -->
+  <h2 class="wp-block-heading alignfull has-white-color has-text-color" style="font-size:9rem"><a
+      href="https://fukuo.site">Tumblr</a></h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:heading {"align":"full","style":{"typography":{"fontSize":"9rem"}},"textColor":"white"} -->
+  <h2 class="wp-block-heading alignfull has-white-color has-text-color" style="font-size:9rem"><a
+      href="https://read.cv/fukuo">Curriculum Vitae</a></h2>
+  <!-- /wp:heading -->
+</div>
+<!-- /wp:group -->
