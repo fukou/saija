12ab62599095d0478b9b11ce2b9ac3f311a944b6
Faiz <faiz.j@softwareseni.com>
2024-07-25 21:21:42 +0700

    feat(patterns): Adjust the text intro block patterns

--- patterns/text-intro.php
+++ patterns/text-intro.php
@@ -8,50 +8,39 @@
  */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"color":{"gradient":"radial-gradient(rgba(190,242,250,0.6) 4%,rgb(249,249,249) 56%)"}},"layout":{"type":"constrained","contentSize":"","wideSize":""}} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"color":{"gradient":"radial-gradient(rgba(190,242,250,0.6) 4%,rgb(249,249,249) 56%)"}},"layout":{"type":"constrained","contentSize":"","wideSize":""}} -->
 <div class="wp-block-group alignfull has-background"
-  style="background:radial-gradient(rgba(190,242,250,0.6) 4%,rgb(249,249,249) 56%);padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
-  <!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"constrained"}} -->
+  style="background:radial-gradient(rgba(190,242,250,0.6) 4%,rgb(249,249,249) 56%);padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--50)">
+  <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"}} -->
   <div class="wp-block-group">
     <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-    <div class="wp-block-group">
-      <!-- wp:paragraph {"align":"left","className":"is-style-asterisk","fontSize":"medium"} -->
-      <p class="has-text-align-left is-style-asterisk has-medium-font-size">Hey, My name is <strong>Faiz</strong>!</p>
+    <div class="wp-block-group"><!-- wp:paragraph {"align":"left","fontSize":"medium"} -->
+      <p class="has-text-align-left has-medium-font-size">Hey, My name is <strong>Faiz</strong>!</p>
       <!-- /wp:paragraph -->
 
-      <!-- wp:heading {"textAlign":"left","level":1,"className":"is-style-default","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"x-large"} -->
-      <h1 class="wp-block-heading has-text-align-left is-style-default has-x-large-font-size"
-        style="font-style:normal;font-weight:600">A front-end web developer, passionate about building good designs and apps that are accessible to everyone.</h1>
+      <!-- wp:heading {"textAlign":"left","level":1,"className":"is-style-default","style":{"typography":{"fontSize":"2.9rem"}}} -->
+      <h1 class="wp-block-heading has-text-align-left is-style-default" style="font-size:2.9rem">A front-end web developer, passionate about building good designs and apps that are accessible to everyone.</h1>
       <!-- /wp:heading -->
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:spacer {"height":"1.25rem"} -->
-    <div style="height:1.25rem" aria-hidden="true" class="wp-block-spacer"></div>
-    <!-- /wp:spacer -->
+    <!-- wp:group {"style":{"spacing":{"blockGap":"0.65rem"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+    <div class="wp-block-group"><!-- wp:paragraph {"align":"left"} -->
+      <p class="has-text-align-left">As a developer &amp; design enthusiast, I'm all about crafting remarkable, high-quality designs. I truly believe that design can transform and enhance our surroundings. <strong>I'm currently diving deeper into UX design and accessibility.</strong></p>
+      <!-- /wp:paragraph -->
 
-    <!-- wp:paragraph {"align":"left"} -->
-    <p class="has-text-align-left">As a developer &amp; design enthusiast, I'm all about crafting remarkable, high-quality designs. I truly believe that design can transform and enhance our surroundings. <strong>I'm currently diving deeper into UX design and accessibility.</strong></p>
-    <!-- /wp:paragraph -->
+      <!-- wp:group {"style":{"border":{"left":{"color":"var:preset|color|secondary","width":"3px"},"top":[],"right":[],"bottom":[]},"spacing":{"padding":{"right":"var:preset|spacing|10","left":"var:preset|spacing|10"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+      <div class="wp-block-group"
+        style="border-left-color:var(--wp--preset--color--secondary);border-left-width:3px;padding-right:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)">
+        <!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary"} -->
+        <p class="has-text-align-left has-secondary-color has-text-color has-link-color">Currently not available for freelance work</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
-
-  <!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"constrained"}} -->
-  <div class="wp-block-group"><!-- wp:spacer {"height":"1.25rem"} -->
-    <div style="height:1.25rem" aria-hidden="true" class="wp-block-spacer"></div>
-    <!-- /wp:spacer -->
-  </div>
-  <!-- /wp:group -->
-
-  <!-- wp:group {"style":{"border":{"left":{"color":"var:preset|color|secondary","width":"3px"},"top":{},"right":{},"bottom":{}},"spacing":{"padding":{"right":"var:preset|spacing|10","left":"var:preset|spacing|10"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-  <div class="wp-block-group"
-    style="border-left-color:var(--wp--preset--color--secondary);border-left-width:3px;padding-right:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)">
-    <!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|accent-3"}}}},"textColor":"accent-3"} -->
-    <p class="has-text-align-left has-accent-3-color has-text-color has-link-color">Currently not available for freelance work</p>
-    <!-- /wp:paragraph -->
-  </div>
-  <!-- /wp:group -->
-
 
   <!-- wp:spacer {"height":"var:preset|spacing|30","style":{"layout":[]}} -->
   <div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
